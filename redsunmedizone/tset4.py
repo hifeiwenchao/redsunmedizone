9d22982cd8f9821ae4bd597e332a3d8daaeb1a0c
yangyun <yangyun0602@126.com>
2016-02-14 13:15:57 +0800

    fix

--- redsunmedizone/tset4.py
+++ redsunmedizone/tset4.py
@@ -6,13 +6,11 @@ Created on 2016年2月13日
 from imapclient.imapclient import IMAPClient
 
 
-
 client = IMAPClient('imap.alibaba.com',ssl=False)
 client.login('matt@heershi.com.cn', 'as501180920')
 folders_list = client.list_folders()
 for item in folders_list:
     print(item)
-
 
 
 
