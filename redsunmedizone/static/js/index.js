672b79765466133f2a0a1a67aea15be854a170f0
yangyun <yangyun0602@126.com>
2016-02-15 15:25:02 +0800

    memo页面

--- redsunmedizone/static/js/index.js
+++ redsunmedizone/static/js/index.js
@@ -105,10 +105,17 @@ $(function($) {
         },
 	});
 	
-	
-	
+	//添加备忘录
+	$('a[data=addMemo]').click(function(){
+		var options = $('div[data=memoCalendar]').calendar('options')
+		console.log(options.current.Format("yyyy-MM-dd"))
+	});
 });
-	
+
+function getMemo(data){
+	console.log(date.Format("yyyy-MM-dd"))
+}
+
 
 
 function unSelect(select){
@@ -125,7 +132,21 @@ function fixWidth(percent){
 }
 
 
-
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
 
 
 
