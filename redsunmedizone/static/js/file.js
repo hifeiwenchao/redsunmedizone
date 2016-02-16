55fd3adfad418abc0659562e73391414949cc916
yangyun <yangyun0602@126.com>
2016-02-16 22:15:53 +0800

    fix

--- redsunmedizone/static/js/file.js
+++ redsunmedizone/static/js/file.js
@@ -39,7 +39,7 @@ $(function($){
 });
 
 function ReloadStaticFileTree(){
-	$.ajax({url:'/static_file_tree/',type:'GET',dataType:'json',success:function(data){
+	$.ajax({url:'/file_tree/',type:'GET',dataType:'json',success:function(data){
 		data[0].state = 'open'
 		$('#static_file_tree').tree('loadData',data);
 	},error:function(data){
@@ -48,7 +48,7 @@ function ReloadStaticFileTree(){
 }
 
 function ReloadStaticFileTreeAll(){
-	$.ajax({url:'/static_file_tree/',data:{'type':'all'},type:'GET',dataType:'json',success:function(data){
+	$.ajax({url:'/file_tree/',data:{'type':'all'},type:'GET',dataType:'json',success:function(data){
 		data[0].state = 'open'
 		$('#static_file_tree_all').tree('loadData',data);
 	},error:function(data){
